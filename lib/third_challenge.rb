abe457bc4560e1dba350e49e01441b5de302e379
zahrank21 <zahrank@gmail.com>
2017-11-26 13:48:26 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,16 +10,20 @@ def third_challenge
         age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo",
+        age: "15"
+        status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet",
+        age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
