f5ecd9bf68ad6f48b8633532b0b57c54a45a18e3
zahrank21 <zahrank@gmail.com>
2017-11-26 13:57:36 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,7 +27,7 @@ def third_challenge
         },
       :heroine => {
         name: "Juliet",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :heroine_friends => []
